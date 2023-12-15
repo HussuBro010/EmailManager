ae876f312953edfc9b9bc5693e1e9fa07ac3ce35
HussuBro010 <hussainv2807@gmail.com>
2023-12-15 21:48:08 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pyfiglet
+import inquirer
+import json
+
+title = pyfiglet.figlet_format("Email Manager", font="digital")
+print(title)
+
+jsonDict = {}
+
+
+def addEmail(label: str, email: str, pswd: str):
+    jsonDict[label] = {"Email": email, "Password": pswd}
+    f = open(f"emailDatas/{label}.json", "a")
+    f.write(json.dumps(jsonDict, indent=4))
+
+
+
+while True:
+    modeOptionsInquirer = [
+        inquirer.List(
+            'modes',
+            message="Select a mode",
+            choices=['Add Email', 'Delete Email', 'Edit Email', 'Quit']
+        )
+    ]
+
+    modes = inquirer.prompt(modeOptionsInquirer)
+
+    if modes['modes'] == 'Add Email':
+        label = input('Name: ')
+        email = input('Email: ')
+        password = input('Password: ')
+        print("\n\n")
+
+        addEmail(label, email, password)
+    if modes['modes'] == 'Quit':
+        break
